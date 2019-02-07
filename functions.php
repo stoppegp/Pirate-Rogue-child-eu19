a63667cd0d1629da4e7e4c13969e7868e5308754
stoppegp <github@ma.stoppe-gp.de>
2019-02-07 08:18:57 +0100

    add slider shortcode

--- functions.php
+++ functions.php
@@ -8,7 +8,33 @@ add_action( 'wp_enqueue_scripts', 'parent_enqueue_styles' );
 
 /* function to run after parents function.php */
 function late_child_function() {
-	// do stuff
+	
+	/* add shortcode for slider */
+	if (!function_exists('pirate_rogue_shortcode_slider') && !function_exists('pirate_rogue_shortcode_slider_entry')) {
+		function pirate_rogue_shortcode_slider( $atts, $content = null ) {
+			// pirate-rogue-script must be enqueued after slick
+			wp_dequeue_script('pirate-rogue-script');
+			wp_enqueue_script('pirate-rogue-slick');
+			wp_enqueue_script('pirate-rogue-script');
+
+			$out = '<div class="featured-slider cf">'.do_shortcode( $content ).'</div>';
+			return $out;
+		}
+		function pirate_rogue_shortcode_slider_entry( $atts, $content = null ) {
+			extract(shortcode_atts(array(
+				'url'	=> '',
+				'img'	=> '',
+				'alt'   => '',
+			), $atts));
+			$url =  esc_attr($url);
+			$img =  esc_attr($img);
+			$alt =  esc_attr($alt);
+			$out = '<article><div class="entry-thumbnail" role="presentation"><a href="'.$url.'"><img src="'.$img.'" alt="'.$alt.'"></a></div></article>';
+			return $out;
+		}
+		add_shortcode('slider', 'pirate_rogue_shortcode_slider');
+		add_shortcode('slider-entry', 'pirate_rogue_shortcode_slider_entry');
+	}
 }
 add_action( 'after_setup_theme', 'late_child_function' );
 ?>
